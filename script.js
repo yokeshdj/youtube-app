0fb262f1b5398ecad0f51cfa0f218d4a2798b7ec
Yokesh <eryokeshuma@gmail.com>
2021-10-16 13:13:17 +0530

    Homepage Updated

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 window.addEventListener("load", function () {
     var submitBtn = document.getElementById("sbm")
-    submitBtn.addEventListener("click", ()=>handleSearch());
+    submitBtn.addEventListener("click", () => handleSearch());
+    getYoutubeData()
+    .then((res)=>createVideo(res.items));
 })
 const types = {
     channel: "youtube#channel",
@@ -9,7 +11,11 @@ const types = {
 Object.freeze(types);
 
 function getYoutubeData(q) {
-    return fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${q}&key=AIzaSyC46DMnWd3yBFqtALl5ygSLzEdNXC-AdH8&maxResults=30`)
+    if (q) {
+        return fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${q}&key=AIzaSyC46DMnWd3yBFqtALl5ygSLzEdNXC-AdH8&maxResults=30`)
+            .then(res => res.json())
+    }
+    return fetch(`https://youtube.googleapis.com/youtube/v3/search?maxResults=30&key=AIzaSyC46DMnWd3yBFqtALl5ygSLzEdNXC-AdH8&chart=mostPopular`)
         .then(res => res.json())
 }
 
@@ -29,31 +35,35 @@ function createYoutubeCards(data) {
     return div;
 }
 
-async function handleSearch(){
+function createVideo(results) {
+    let count = 0;
+    const allCards = [];
+    for (let video of results) {
+        const card = createYoutubeCards(video);
+        if (card && count < 20) {
+            allCards.push(card);
+            count++
+        }
+    }
+    const resContainer = document.getElementById("container");
+    resContainer.innerHTML = null;
+    resContainer.append(...allCards);
+}
+async function handleSearch() {
     // e.preventDefault();
     const search = document.getElementById("input").value;
     try {
         const {
-            items : results,
-            pageInfo : {
+            items: results,
+            pageInfo: {
                 resultsPerPage,
                 totalResults
             }
         } = await getYoutubeData(search);
-        console.log(results,resultsPerPage,totalResults)
-        let count = 0;
-        const allCards = [];
-        for(let video of results){
-            const card = createYoutubeCards(video);
-            if(card && count<20){
-                allCards.push(card);
-                count++
-            }
-        }
-        const resContainer = document.getElementById("container");
-        resContainer.innerHTML = null;
-        resContainer.append(...allCards);
+        console.log(results, resultsPerPage, totalResults)
+        createVideo(results);
+
     } catch (error) {
-        
+
     }
 }
